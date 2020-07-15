1c92ea25c4e6d1e3ba2bdb56bcbfb75d893f4c40
Rose Kairu <rosekairu@gmail.com>
2020-07-15 20:20:31 +0300

    flashcard project

---
+++ flashcards/forms.py
@@ -0,0 +1,45 @@
+from .models import Card, Deck
+from django.forms import ModelForm, Textarea, inlineformset_factory, TextInput
+from django.contrib.auth.models import User
+from django.views.generic.edit import CreateView, UpdateView
+
+
+# Create the form class.
+class CardForm(ModelForm):
+    class Meta:
+        model = Card
+        fields = ('question', 'answer', 'deck')
+
+class DeckForm(ModelForm):
+    class Meta:
+        model = Deck
+        fields = ('title', 'subject', 'description')
+        widgets = {'title':TextInput(attrs={'class': 'form-control','placeholder': "Title",}),
+                    'subject':
+                                TextInput(attrs={
+                                'class': 'form-control',
+                                'placeholder': "Subject e.g. Biology",
+                                }),
+                    'description':
+                                Textarea(attrs={
+                                'class': 'form-control',
+                                'placeholder': "Description e.g. Molecular Biology of the Cell, chapter 5-7",
+                                }),
+                    }
+
+
+CardFormSet = inlineformset_factory(
+                Deck, Card,
+                fields=('question','answer'),
+                can_delete=False,
+                extra=1,
+                widgets={
+                    'question': TextInput(attrs={"class":"form-control form-control-lg",
+                                                    "placeholder":"Question",}),
+                    'answer': TextInput(attrs={"class":"form-control",
+                                                    "placeholder":"Answer"}),
+
+
+                        }
+
+                )
---
+++ flashcards/urls.py
@@ -0,0 +1,24 @@
+from django.urls import path
+from . import views
+from .views import DeckCreate
+
+
+# app_name = 'cardmaker'
+
+urlpatterns = [
+    path('', views.show_all_decks, name='all_decks'),
+    path('', views.signup, name='sign_up'),
+    path('card/all', views.show_all_cards, name='all_cards'),
+    path('card/delete/<int:card_id>/', views.delete_card, name="delete_card"),
+    path('deck/all', views.show_all_decks, name='all_decks'),
+    path('deck/delete/<int:deck_id>/', views.delete_deck, name="delete_deck"),
+    path('deck/create/', views.DeckCreate.as_view(), name='create_deck'),
+    path('deck/<deck_id>/', views.create_cards, name='create_cards'),
+    path('deck/show/<int:deck_id>/', views.show_deck, name="show_deck"),
+    path('deck/rehearse/<int:deck_id>/', views.rehearse_deck, name="rehearse_deck"),
+    path('deck/save/<int:deck_id>/', views.save_for_study, name="save_for_study"),
+    path('studyset/', views.show_my_studyset, name='show_my_studyset'),
+    path('studyset/remove/<int:studentdeck_id>/',views.remove_from_studyset, name= 'remove_from_studyset'),
+    path('deck/flip/<int:deck_id>', views.flip, name='flip')
+
+]
